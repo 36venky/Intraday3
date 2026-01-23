b938d4baa3a5532f6c0045fe06c91029e0c32147
36Venky <venkateshraju761@gmail.com>
2026-01-23 07:29:49 +0530

    Cleaning fixed

--- CleanUp.py
+++ CleanUp.py
@@ -1,8 +1,5 @@
 import os
 import glob
-import shutil
-
-EXCLUDE_DIRS = {"intraday", "venv", ".venv", "Lib", "site-packages"}
 
 def cleanup_project(folder_path):
     # Delete txt and log files (top-level only)
@@ -13,25 +10,3 @@ def cleanup_project(folder_path):
                 print(f"Deleted file: {file}")
             except Exception as e:
                 print(f"Failed to delete file {file}: {e}")
-
-    # Delete __pycache__ safely
-    for root, dirs, _ in os.walk(folder_path):
-        if any(excl in root for excl in EXCLUDE_DIRS):
-            continue
-
-        for d in dirs:
-            if d == "__pycache__":
-                path = os.path.join(root, d)
-                try:
-                    shutil.rmtree(path)
-                    print(f"Deleted folder: {path}")
-                except Exception as e:
-                    print(f"Failed to delete folder {path}: {e}")
-
-# Usage
-cleanup_project(r"C:\Users\91702\OneDrive\Desktop\Intraday")
-cleanup_project(r"C:\Users\91702\OneDrive\Desktop\Intraday\Modules")
-cleanup_project(r"C:\Users\91702\OneDrive\Desktop\Intraday\Modules\logs")
-cleanup_project(r"C:\Users\91702\OneDrive\Desktop\Intraday\Dependencies")
-cleanup_project(r"C:\Users\91702\OneDrive\Desktop\Intraday\Indicators")
-
